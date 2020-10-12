b06c7e475e8e20733294cd73d1d925f7c5ef7f89
astnreaperx <austin.slota@gmail.com>
2020-10-12 15:36:33 -0500

    Added controller for Type with show and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'type/index'
+  get 'type/show'
   get "/pokemon", to: "pokemon#index", as: "pokemons"
   get "/pokemon/:id", to: "pokemon#show", as: "pokemon", constraints: { id: /\d+/ } # company_path
 
